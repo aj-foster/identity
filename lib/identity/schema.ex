c8dc02074a4d9175b705a5d12fc4ea8531ac58c9
AJ Foster <2789166+aj-foster@users.noreply.github.com>
2022-07-17 13:49:55 -0400

    Add associations to user schema

---
+++ lib/identity/schema.ex
@@ -0,0 +1,48 @@
+defmodule Identity.Schema do
+  @moduledoc """
+  Provides helpers for writing custom schemas.
+
+  Out of the box, Identity provides the following schemas:
+
+    * `Identity.User`, representing an individual using the application with multiple login methods,
+      email addresses, and sessions.
+    * `Identity.Schema.BasicLogin`, representing a password login method.
+    * `Identity.Schema.Email`, representing an email address used for logins, notifications, etc.
+    * `Identity.Schema.PasswordToken`, representing a password reset token.
+    * `Identity.Schema.Session`, representing an active login session.
+
+  The schemas under `Identity.Schema.*` are fully managed by Identity and its migrations. If you
+  find yourself working with this struct directly or changing the underlying table, please share
+  your use case with the maintainers of the library.
+  """
+
+  @doc """
+  Define Ecto associations for a custom User schema.
+
+  If using a custom schema to replace `Identity.User`, calling this macro in the body of `schema`
+  ensures the User is properly associated with Identity's internal schemas. This is necessary for
+  efficient queries and easy preloading of data within Identity's core functions.
+
+  ## Example
+
+      defmodule MyApp.User do
+        use Ecto.Schema
+        import Identity.Schema
+
+        @foreign_key_type :binary_id
+        @primary_key {:id, :binary_id, autogenerate: true}
+        schema "users" do
+          # ...
+          user_associations()
+        end
+      end
+  """
+  defmacro user_associations do
+    quote do
+      Ecto.Schema.has_many(:emails, Identity.Schema.Email, foreign_key: :user_id)
+      Ecto.Schema.has_one(:logins, Identity.Schema.BasicLogin, foreign_key: :user_id)
+      Ecto.Schema.has_one(:password_token, Identity.Schema.PasswordToken, foreign_key: :user_id)
+      Ecto.Schema.has_many(:sessions, Identity.Schema.Session, foreign_key: :user_id)
+    end
+  end
+end
--- lib/identity/user.ex
+++ lib/identity/user.ex
@@ -1,12 +1,55 @@
 defmodule Identity.User do
-  @moduledoc "Struct representing a person in the real world."
+  @moduledoc """
+  Struct representing a person in the real world.
+
+  Identity provides `Identity.User` as a minimal schema to get started. It has a binary (UUID)
+  primary key, and a collection of associations.
+
+  ## Replacing this Schema
+
+  Often, developers will want to store additional information related to users. This is possible
+  using the `user` config (see `Identity.Config`). However, before adding a new field to the user
+  schema, consider:
+
+    1. Does Identity provide the same information in an associated struct already?
+    2. Would an associated record be more appropriate?
+
+  Keeping the user schema simple may increase the flexibility of the data in the future.
+
+  ### Requirements
+
+  Any custom schema should conform to two facts:
+
+    1. It must have a binary (UUID) primary key called `:id`, and
+    2. It must call the `Identity.Schema.user_associations/0` macro.
+
+  Following is a minimal User schema:
+
+      defmodule Identity.User do
+        use Ecto.Schema
+        import Identity.Schema
+
+        @primary_key {:id, :binary_id, autogenerate: true}
+        schema "users" do
+          # ...
+          user_associations()
+        end
+      end
+  """
   use Ecto.Schema
+  import Identity.Schema
 
   @type t :: %__MODULE__{
-          id: Ecto.UUID.t()
+          emails: Ecto.Schema.has_many(Identity.Schema.Email.t()),
+          id: Ecto.UUID.t(),
+          logins: Ecto.Schema.has_one(Identity.Schema.BasicLogin.t() | nil),
+          password_token: Ecto.Schema.has_one(Identity.Schema.PasswordToken.t() | nil),
+          sessions: Ecto.Schema.has_many(Identity.Schema.Session.t())
         }
 
   @foreign_key_type :binary_id
   @primary_key {:id, :binary_id, autogenerate: true}
-  schema("users", do: [])
+  schema "users" do
+    user_associations()
+  end
 end
