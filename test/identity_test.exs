05c4205d9ac162833b67bf2e1a4b3589a211d74b
AJ Foster <2789166+aj-foster@users.noreply.github.com>
2022-07-23 20:21:10 -0400

    Move changeset error helper

--- test/identity_test.exs
+++ test/identity_test.exs
@@ -757,12 +757,4 @@ defmodule IdentityTest do
       assert [_, _] = Repo.all(Session)
     end
   end
-
-  def errors_on(changeset) do
-    Ecto.Changeset.traverse_errors(changeset, fn {message, opts} ->
-      Regex.replace(~r"%{(\w+)}", message, fn _, key ->
-        opts |> Keyword.get(String.to_existing_atom(key), key) |> to_string()
-      end)
-    end)
-  end
 end
