9559fe7a930fd76ea487c7ed66aec0872e6d723b
AJ Foster <2789166+aj-foster@users.noreply.github.com>
2022-07-30 23:14:01 -0400

    Add Heex formatter

--- .formatter.exs
+++ .formatter.exs
@@ -1,6 +1,7 @@
 # Used by "mix format"
 [
   import_deps: [:ecto, :phoenix, :plug],
-  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
+  inputs: ["{mix,.formatter}.exs", "{config,lib,priv,test}/**/*.{ex,exs,heex}"],
+  plugins: [Phoenix.LiveView.HTMLFormatter],
   subdirectories: ["lib/migrations"]
 ]
