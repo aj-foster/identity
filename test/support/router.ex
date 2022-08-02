6952ab4add796c53d9cfaccca160733433de5cbf
AJ Foster <2789166+aj-foster@users.noreply.github.com>
2022-08-01 21:11:14 -0400

    Reorganize router

--- test/support/router.ex
+++ test/support/router.ex
@@ -3,22 +3,40 @@ if Code.ensure_loaded?(Phoenix.Router) do
     use Phoenix.Router
 
     scope "/" do
+      #
+      # Session
+      #
+
       get "/session/new", Identity.Controller, :new_session, as: :identity
       post "/session/new", Identity.Controller, :create_session, as: :identity
-      delete "/session", Identity.Controller, :delete_session, as: :identity
 
       get "/session/2fa", Identity.Controller, :pending_2fa, as: :identity
       post "/session/2fa", Identity.Controller, :validate_2fa, as: :identity
 
+      delete "/session", Identity.Controller, :delete_session, as: :identity
+
+      #
+      # Password Reset
+      #
+
       get "/password/new", Identity.Controller, :new_password_token, as: :identity
       post "/password/new", Identity.Controller, :create_password_token, as: :identity
+
       get "/password/:token", Identity.Controller, :new_password, as: :identity
       put "/password/:token", Identity.Controller, :create_password, as: :identity
+
+      #
+      # Email Addresses
+      #
 
       get "/email/new", Identity.Controller, :new_email, as: :identity
       post "/email/new", Identity.Controller, :create_email, as: :identity
       get "/email/:token", Identity.Controller, :confirm_email, as: :identity
       delete "/user/email", Identity.Controller, :delete_email, as: :identity
+
+      #
+      # User Settings
+      #
 
       get "/user/new", Identity.Controller, :new_user, as: :identity
       post "/user/new", Identity.Controller, :create_user, as: :identity
